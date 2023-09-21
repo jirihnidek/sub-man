10983ef03022b0e063f015080feacd2d2a127f16
Jiri Hnidek <jhnidek@redhat.com>
2023-09-21 16:22:46 +0200

    Fix issue with parsing status.

--- status.go
+++ status.go
@@ -4,21 +4,24 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 // RHSMStatus is structure used for storing GET response from REST API
 // endpoint "/status". This endpoint can be called using no-auth or
 // consumer-cert-auth connection
 type RHSMStatus struct {
-	Mode                string      `json:"mode"`
-	ModeReason          interface{} `json:"modeReason"`
-	ModeChangeTime      interface{} `json:"modeChangeTime"`
-	Result              bool        `json:"result"`
-	Version             string      `json:"version"`
-	Release             string      `json:"release"`
-	Standalone          bool        `json:"standalone"`
-	TimeUTC             time.Time   `json:"timeUTC"`
+	Mode           string      `json:"mode"`
+	ModeReason     interface{} `json:"modeReason"`
+	ModeChangeTime interface{} `json:"modeChangeTime"`
+	Result         bool        `json:"result"`
+	Version        string      `json:"version"`
+	Release        string      `json:"release"`
+	Standalone     bool        `json:"standalone"`
+	// Note: json module cannot unmarshal timeUTC into time.Time
+	// for this reason: https://github.com/golang/go/issues/47353
+	// Because we do not need to use timeUTC for anything ATM.
+	// It is parsed as normal string.
+	TimeUTC             string      `json:"timeUTC"`
 	RulesSource         string      `json:"rulesSource"`
 	RulesVersion        string      `json:"rulesVersion"`
 	ManagerCapabilities []string    `json:"managerCapabilities"`
